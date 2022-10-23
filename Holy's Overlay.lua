e8b64b6ae5c2d9e253f8929a4069f2eb63cc962b
Holy <74293660+HolyJoey@users.noreply.github.com>
2022-10-23 15:41:33 +0200

    Update Holy's Overlay.lua

--- Holy's Overlay.lua
+++ Holy's Overlay.lua
@@ -1098,6 +1098,7 @@ menu.toggle(info_root, "Info Overlay", {"InfoOverlay"}, "Info overlay in a cute 
     end
 end)
 
+-- Cry about it
 util.show_corner_help("~s~Enjoy~h~~r~ " .. SCRIPT_FILENAME)
 util.on_stop(function()
     util.show_corner_help("~s~Imagine closing me")
